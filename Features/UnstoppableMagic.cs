bcf88399ca09e18f44694ab3fa44de75447ffdd3
Aaron Maslen <aaron.maslen@gmail.com>
2022-05-14 18:28:15 +1000

    derp

--- Features/UnstoppableMagic.cs
+++ Features/UnstoppableMagic.cs
@@ -41,11 +41,6 @@ namespace AlternateHumanTraits.Features
 				{
 					prerequisite.HideInUI = true;
 				}, removeOnApply: true);
-<<<<<<< HEAD
-=======
-
-				//feat.AddFeatureCallback(new FeatureNeedsUpdate());
->>>>>>> 39ff1f0fc89ffd4726196207e32a90bcb96309cb
 			});
 		}
 	}
